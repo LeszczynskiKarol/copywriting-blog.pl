6649b96aa48231f66875d19cd7b5fb09c5a90341
LeszczynskiKarol <leszczu123@wp.pl>
2026-02-12 11:30:05 +0100

    gotowe wszystko

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
-// d:\copywriting-blog.pl\astro.config.mjs
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://www.copywriting-blog.pl",
+  integrations: [
+    sitemap({
+      filter: (page) => 
+        !page.includes('/polityka-prywatnosci'),
+    }),
+  ],
 });
